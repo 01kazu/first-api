ef4312bcc5a8a0c1c325522c6cd923b20ee8bc96
Joshua Yakubu David <30726966+01kazu@users.noreply.github.com>
2025-02-01 01:04:46 +0100

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ def get_current_datetime_iso():
     # Get current datetime for UTC
     utc_dt = datetime.now(timezone.utc)
     # Convert UTC time to ISO 8601 format
-    iso_date = utc_dt.isoformat(timespec="seconds")
+    iso_date = utc_dt.isoformat(timespec="seconds").replace('+00:00', 'Z')
     return iso_date
 
 
